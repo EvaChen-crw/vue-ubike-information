870338b176cb0aa3b00fd91138e9b4a793a2072d
Eva Chen <77389123+EvaChen-crw@users.noreply.github.com>
2021-01-21 20:07:16 +0800

    Create Search.vue

---
+++ ubike-app/src/components/Search.vue
@@ -0,0 +1,35 @@
+<template>
+    <p>
+      站點名稱搜尋: <input type="text" v-model="searchName">
+    </p>
+</template>
+
+<script>
+export default {
+  props:{
+    parentUbikestops: {
+      type: Array,
+      required: true
+    }
+  },
+  data () {
+    return {
+      searchName: '',
+      currentPage: 0,  
+    }
+  },
+  watch: {
+    searchStopName(){
+      if (this.searchName !== ''){
+        this.currentPage = 1;
+      }      
+      this.$emit("update", this.searchStopName, this.currentPage);
+    }
+  },
+  computed: {
+    searchStopName(){
+      return this.parentUbikestops.filter(u => u.sna.toLowerCase().includes(this.searchName));
+    },
+  },
+}
+</script>
